c9cb3f2b60bd5a578bccf72319e7c798e297e404
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2022-06-01 23:45:10 -0500

    update logic accept differents fonts

--- modules/modules.py
+++ modules/modules.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+import os
 
 # crear lienzo imagen
 def createBaseImage(size_y, size_x, url_save):
@@ -14,22 +15,24 @@ def overlayImage(imgOverlap, position, url_image):
     image.save(url_image)
 
 # agregar texto a imagen
-def addTextImage(img, text, fontSize, fontColor, fontWeigth, marginTop):
-    font = cv2.FONT_HERSHEY_DUPLEX
+def addTextImage(url_image, text, fontSize, fontColor, marginTop, font):
+    img = cv2.imread(url_image)
     width = img.shape[1]
     heigth = img.shape[0]
+    font_extra = ImageFont.truetype(font, fontSize)
     # separar por saltos de linea
     for i, line in enumerate(text.split('\n')):
         text_formatter = line.strip()
-        textsize = cv2.getTextSize(
-            text_formatter, font, fontSize, fontWeigth)[0]
-        textX = int((width - textsize[0]) / 2)
+        image = Image.open(url_image)
+        draw = ImageDraw.Draw(im=image)
+        textX = int((width) / 2)
         # imprimir primera linea
         if(i == 0):
             textY = int(heigth * marginTop)
         else:
             textY = int(heigth * marginTop) + \
-                textsize[1] * i + int(textsize[1] * 0.8 * i)
+                fontSize * i + int(fontSize * 0.8 * i)
         position = (textX, textY)
-        cv2.putText(img, text_formatter, position, font,
-                    fontSize, fontColor, fontWeigth)
+        draw.text(xy=position, text=text_formatter,
+                  font=font_extra, fill=fontColor, anchor='mm')
+        image.save(url_image)
--- modules/sizes.py
+++ modules/sizes.py
@@ -4,50 +4,62 @@ def sizesAndroid():
         {
             "name": "drawable-land-hdpi",
             "size": "800x480",
+            "fontSize": 25,
         },
         {
             "name": "drawable-land-ldpi",
             "size": "320x200",
+            "fontSize": 15,
         },
         {
             "name": "drawable-land-mdpi",
             "size": "480x320",
+            "fontSize": 23,
         },
         {
             "name": "drawable-land-xhdpi",
             "size": "1280x720",
+            "fontSize": 40,
         },
         {
             "name": "drawable-land-xxhdpi",
             "size": "1600x960",
+            "fontSize": 50,
         },
         {
             "name": "drawable-land-xxxhdpi",
             "size": "1920x1080",
+            "fontSize": 65,
         },
         {
             "name": "drawable-port-hdpi",
             "size": "480x800",
+            "fontSize": 25,
         },
         {
             "name": "drawable-port-ldpi",
             "size": "200x320",
+            "fontSize": 10,
         },
         {
             "name": "drawable-port-mdpi",
             "size": "320x480",
+            "fontSize": 20,
         },
         {
             "name": "drawable-port-xhdpi",
             "size": "720x1280",
+            "fontSize": 40,
         },
         {
             "name": "drawable-port-xxhdpi",
             "size": "960x1600",
+            "fontSize": 55,
         },
         {
             "name": "drawable-port-xxxhdpi",
             "size": "1080x1920",
+            "fontSize": 70,
         },
     ]
 
@@ -57,43 +69,56 @@ def sizesIos():
         {
             "name": "Default-568h@2x.png",
             "size": "640x1136",
+            "fontSize": 40,
         },
         {
             "name": "Default-667h@2x.png",
             "size": "750x1334",
+            "fontSize": 45,
         },
         {
             "name": "Default-Landscape-736h@3x.png",
             "size": "2208x1242",
+            "fontSize": 60,
         },
         {
             "name": "Default-Landscape-812h@3x.png",
             "size": "2436x1125",
-        }, {
+            "fontSize": 70,
+        }, 
+        {
             "name": "Default-Landscape.png",
             "size": "1024x768",
+            "fontSize": 40,
         },
         {
             "name": "Default-Landscape@2x.png",
             "size": "2048x1536",
+            "fontSize": 65,
         },
         {
             "name": "Default-Portrait-736h@3x.png",
             "size": "1242x2208",
-        }, {
+            "fontSize": 70,
+        }, 
+        {
             "name": "Default-Portrait-812h@3x.png",
             "size": "1125x2436",
+            "fontSize": 70,
         },
         {
             "name": "Default-Portrait.png",
             "size": "768x1024",
+            "fontSize": 40,
         },
         {
             "name": "Default-Portrait@2x.png",
             "size": "1536x2048",
+            "fontSize": 80,
         },
         {
             "name": "Default@2x.png",
             "size": "640x960",
+            "fontSize": 40,
         }
     ]
